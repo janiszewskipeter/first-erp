019209cef11d77b38b4fe00c2880087d156ec1f1
Piotr Janiszewski <janiszewskipeter@gmail.com>
2020-04-01 13:17:06 +0200

    unify get_data func in crm,hr,sales

--- erp.py
+++ erp.py
@@ -1,16 +1,10 @@
 from controller import main_controller
-<<<<<<< HEAD
 import model.hr.hr
 
 
-#if __name__ == '__main__':
- #   main_controller.menu()
-
 print(model.hr.hr.get_data())
-=======
 import model.sales.sales
 import model.crm.crm
 
 #if __name__ == '__main__':
     # main_controller.menu()
->>>>>>> 08cb8bc819f2a7244e2ed57bee95ebd0863bc1bf
