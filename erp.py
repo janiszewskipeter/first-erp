175510e7d989025db7a2a742ef3e69e0e25bc8af
CecyliaM <cecylia.meredith@gmail.com>
2020-04-01 12:13:01 +0200

    hr update

--- erp.py
+++ erp.py
@@ -1,5 +1,8 @@
 from controller import main_controller
+import model.hr.hr
 
 
-if __name__ == '__main__':
-    main_controller.menu()
+#if __name__ == '__main__':
+ #   main_controller.menu()
+
+print(model.hr.hr.get_data())
